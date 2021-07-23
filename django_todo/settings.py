550ad1d13e75d184aeb723c58edd9167a72e33f3
RuiOliveira83 <rui.p3ld@gmail.com>
2021-07-23 09:23:02 +0000

    Allowed hosts amended on settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', ) 'django-insecure-t^-sy&br5e&^ol&hc1w556cf=a8hd5c3-p7w_y79+x0b^81+_c'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-t^-sy&br5e&^ol&hc1w556cf=a8hd5c3-p7w_y79+x0b^81+_c')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
